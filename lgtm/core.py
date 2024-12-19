3f22ff2ac530a3417dd398f68021967057aa8bd4
hyobin <hyobin90@gmail.com>
2024-12-19 14:32:32 -0500

    Added the prototype

---
+++ lgtm/core.py
@@ -0,0 +1,12 @@
+# an entry point script
+import click
+
+@click.command()
+def cli():
+    """LGtm image generating tool"""
+    lgtm()
+    click.echo('lgtm') # for testing
+
+def lgtm():
+    # logics to be added
+    pass
