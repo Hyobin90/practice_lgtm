cde8222a77e5463a71c022b64b223e2bd61fabb3
hyobin <hyobin90@gmail.com>
2024-12-19 18:52:56 -0500

    Get command line argument

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,13 +1,15 @@
 # an entry point script
 import click
 
+
 @click.command()
-def cli():
-    """LGtm image generating tool"""
+@click.option('--message', '-m', default='LGMT', show_default=True, help='Str to be included in the image.')
+@click.argument('keyword')
+def cli(keyword: str, message: str):
+    """LGTM image generating tool."""
     lgtm()
     click.echo('lgtm') # for testing
 
 def lgtm():
     # logics to be added
     pass
-# hi
